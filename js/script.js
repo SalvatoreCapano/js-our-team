2dff88fbd376a9375679ec5df8fab524b95a23b7
Salvatore Capano <capano_salvatore@libero.it>
2023-01-16 15:50:14 +0100

    Milestone 1 completata: creato array di oggetti con i dati forniti

---
+++ js/script.js
@@ -0,0 +1,47 @@
+/*
+STEPS:
+1. Dichiarare array di oggetti
+2. Inizializzare array con dati membri del team
+    2.1 Nome, ruolo, foto
+3. Stampare in console nome, cognome, ruolo, stringa foto per ogni membro
+4. Stampare nel DOM nome, cognome, ruolo, stringa foto per ogni membro del team
+*/
+
+const team = [
+    {
+        firstName: "Wayne",
+        lastName: "Barnett",
+        role: "Founder & CEO",
+        imgUrl: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        firstName: "Angela",
+        lastName: "Caroll",
+        role: "Chief Editor",
+        imgUrl: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        firstName: "Walter",
+        lastName: "Gordon",
+        role: "Office Manager",
+        imgUrl: "walter-gordon-office-manager.jpgg"
+    },
+    {
+        firstName: "Angela",
+        lastName: "Lopez",
+        role: "Social Media Manager",
+        imgUrl: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        firstName: "Scott",
+        lastName: "Estrada",
+        role: "Developer",
+        imgUrl: " scott-estrada-developer.jpg"
+    },
+    {
+        firstName: "Barbara",
+        lastName: "Ramos",
+        role: "Graphic Designer",
+        imgUrl: "barbara-ramos-graphic-designer.jpg"
+    }
+];
